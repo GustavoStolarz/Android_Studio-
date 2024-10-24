1e5c4c4e5abcd33ec8f4d981fbcae6732f6890ad
Gustavo <gustavo.stolarz@gmail.com>
2024-10-24 15:24:17 -0300

    New Patch

--- project7/app/src/main/java/com/stolarz/project7/MainActivity.java
+++ project7/app/src/main/java/com/stolarz/project7/MainActivity.java
@@ -1,12 +1,18 @@
 package com.stolarz.project7;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void selecionarpedra(View view) {
+        verificarganhador("pedra");
+    }
+
+    public void selecionarpapel(View view) {
+        verificarganhador("papel");
+    }
+
+    public void selecionartesoura(View view) {
+        verificarganhador("tesoura");
+    }
+
+    private String gerarEscolhaAleatoriaAPP(){
+        String[] opcoes = {"pedra","papel","tesoura"};
+        int numeroaletorio = new Random().nextInt(3);
+
+        ImageView imagemapp = findViewById(R.id.lol);
+        String esccolhaapp = opcoes[numeroaletorio];
+
+        switch (esccolhaapp){
+            case "pedra":
+                imagemapp.setImageResource(R.drawable.ped);
+                break;
+            case "papel":
+                imagemapp.setImageResource(R.drawable.image_removebg_preview__1_);
+                break;
+            case "tesoura":
+                imagemapp.setImageResource(R.drawable.image_removebg_preview__2_);
+                break;
+        }
+
+        return esccolhaapp;
+    }
+
+    public void verificarganhador(String escolhadousuario) {
+        System.out.println("Item clicado: " + escolhadousuario);
+
+        String escolhaapp = gerarEscolhaAleatoriaAPP();
+
+        TextView textoresultado = findViewById(R.id.resultado);
+        if (
+                (escolhaapp == "pedra" && escolhadousuario == "tesoura") ||
+                        (escolhaapp == "papel" && escolhadousuario == "pedra")||
+                        (escolhaapp == "tesoura" && escolhadousuario == "papel")
+        ){
+            textoresultado.setText("Você perdeu! ");
+        }else if (
+                (escolhaapp == "tesoura" && escolhadousuario == "pedra") ||
+                        (escolhaapp == "pedra" && escolhadousuario == "papel")||
+                        (escolhaapp == "papel" && escolhadousuario == "tesoura")
+        ) {
+            textoresultado.setText("Você ganhou! ");
+        }else{
+            textoresultado.setText("Você empatou!");
+        }
     }
 }
